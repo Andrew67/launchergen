4761f88613d7a09aa0083861ab943dc4ba61633e
Andrew67 <andres@andrew67.com>
2016-04-16 23:35:28 -0400

    Refactor launcher/generator forms.

--- web/js/main.js
+++ web/js/main.js
@@ -4,52 +4,55 @@
  * Web: https://github.com/Andrew67/launchergen
  */
 
+var launcher = document.forms["launcher"];
+var generator = document.forms["generator"];
+
 var buildExportUrl = function() {
     var url = "";
     url += window.location.href.split("?")[0];
 
     // Effective values, as opposed to generator options input (equal in theory once dynamic)
-    url += "?numapps=" + encodeURIComponent("" + document.forms["launcher"]["name[]"].length);
-    url += "&charset=" + encodeURIComponent(document.forms["launcher"]["charset"].value);
+    url += "?numapps=" + encodeURIComponent("" + launcher["name[]"].length);
+    url += "&charset=" + encodeURIComponent(launcher["charset"].value);
 
     // Launcher Options
-    var launcher = {
-        "header": document.forms["launcher"]["header"].value,
-        "list_header": document.forms["launcher"]["list_header"].value,
-        "prompt": document.forms["launcher"]["prompt"].value,
-        "bgcolor": document.forms["launcher"]["bgcolor"].value,
-        "fgcolor": document.forms["launcher"]["fgcolor"].value,
+    var launcher_state = {
+        "header": launcher["header"].value,
+        "list_header": launcher["list_header"].value,
+        "prompt": launcher["prompt"].value,
+        "bgcolor": launcher["bgcolor"].value,
+        "fgcolor": launcher["fgcolor"].value,
         "apps": []
     };
-    for (var i = 0; i < document.forms["launcher"]["name[]"].length; ++i) {
-        launcher.apps.push({
-            "name": document.forms["launcher"]["name[]"][i].value,
-            "path": document.forms["launcher"]["path[]"][i].value,
-            "flag": document.forms["launcher"]["flag[]"][i].value
+    for (var i = 0; i < launcher["name[]"].length; ++i) {
+        launcher_state.apps.push({
+            "name": launcher["name[]"][i].value,
+            "path": launcher["path[]"][i].value,
+            "flag": launcher["flag[]"][i].value
         });
     }
 
-    console.info(launcher);
-    url += "&launcher=" + encodeURIComponent(JSON.stringify(launcher));
+    console.info(launcher_state);
+    url += "&launcher=" + encodeURIComponent(JSON.stringify(launcher_state));
 
     return url;
 };
 
 var setLauncherCharset = function(charset) {
-    document.forms["launcher"].acceptCharset = charset;
-    document.forms["launcher"]["charset"].value = charset;
+    launcher.acceptCharset = charset;
+    launcher["charset"].value = charset;
 };
 
 // Enable dynamic form elements and bind event handlers
 document.addEventListener("DOMContentLoaded", function() {
     // Export Profile
-    document.forms["launcher"]["export"].disabled = false;
-    document.forms["launcher"]["generate-url"].addEventListener("click", function() {
-        document.forms["launcher"]["export-url"].value = buildExportUrl();
+    launcher["export"].disabled = false;
+    launcher["generate-url"].addEventListener("click", function() {
+        launcher["export-url"].value = buildExportUrl();
     });
 
     // Dynamic "display name encoding"
-    document.forms["generator"]["charset"].addEventListener("change", function() {
+    generator["charset"].addEventListener("change", function() {
         setLauncherCharset(this.value);
     });
 });
